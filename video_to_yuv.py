46c655069dac1ad88acf40b6f44b9c112c9763c3
JL <55872611+jinnliu@users.noreply.github.com>
2021-10-02 12:58:53 +0800

    Add files via upload

--- video_to_yuv.py
+++ video_to_yuv.py
@@ -11,7 +11,7 @@ from common.transformations.camera import transform_img, eon_intrinsics
 from common.transformations.model import medmodel_intrinsics
 '''
 big   RGB (874, 1164, 3) = (H, W, C) => big YUV (1311, 1164) = (X, Y) =>
-small YUV (384, 512) => scYUV (6, 128, 256) = (c, x, y) =>
+small YUV (384, 512) => CsYUV (6, 128, 256) = (C, x, y) =>
 small RGB (256, 512, 3)
 '''
 def RGB_to_sYUVs(video):
@@ -36,35 +36,35 @@ def RGB_to_sYUVs(video):
   print("#---  np.shape(sYUVs) =", np.shape(sYUVs))
   return sYUVs
 
-def sYUVs_to_scYUVs(sYUVs):
+def sYUVs_to_CsYUVs(sYUVs):
   H = (sYUVs.shape[1]*2)//3  # 384x2//3 = 256
   W = sYUVs.shape[2]         # 512
-  scYUVs = np.zeros((sYUVs.shape[0], 6, H//2, W//2), dtype=np.uint8)
+  CsYUVs = np.zeros((sYUVs.shape[0], 6, H//2, W//2), dtype=np.uint8)
 
-  scYUVs[:, 0] = sYUVs[:, 0:H:2, 0::2]  # [2::2] get every even starting at 2
-  scYUVs[:, 1] = sYUVs[:, 1:H:2, 0::2]  # [start:end:step], [2:4:2] get every even starting at 2 and ending at 4
-  scYUVs[:, 2] = sYUVs[:, 0:H:2, 1::2]  # [1::2] get every odd index, [::2] get every even
-  scYUVs[:, 3] = sYUVs[:, 1:H:2, 1::2]  # [::n] get every n-th item in the entire sequence
-  scYUVs[:, 4] = sYUVs[:, H:H+H//4].reshape((-1, H//2, W//2))
-  scYUVs[:, 5] = sYUVs[:, H+H//4:H+H//2].reshape((-1, H//2, W//2))
+  CsYUVs[:, 0] = sYUVs[:, 0:H:2, 0::2]  # [2::2] get every even starting at 2
+  CsYUVs[:, 1] = sYUVs[:, 1:H:2, 0::2]  # [start:end:step], [2:4:2] get every even starting at 2 and ending at 4
+  CsYUVs[:, 2] = sYUVs[:, 0:H:2, 1::2]  # [1::2] get every odd index, [::2] get every even
+  CsYUVs[:, 3] = sYUVs[:, 1:H:2, 1::2]  # [::n] get every n-th item in the entire sequence
+  CsYUVs[:, 4] = sYUVs[:, H:H+H//4].reshape((-1, H//2, W//2))
+  CsYUVs[:, 5] = sYUVs[:, H+H//4:H+H//2].reshape((-1, H//2, W//2))
 
-  scYUVs = np.array(scYUVs).astype(np.float32)/128.0 - 1.0
+  CsYUVs = np.array(CsYUVs).astype(np.float32)/128.0 - 1.0
   # RGB: [0, 255], YUV: [0, 255]/128 - 1 = (-1, 1)
 
-  print("#---  np.shape(scYUVs) =", np.shape(scYUVs))
-  return scYUVs
+  print("#---  np.shape(CsYUVs) =", np.shape(CsYUVs))
+  return CsYUVs
 
 if __name__ == "__main__":
   video_files = sys.argv[1]
   cap = cv2.VideoCapture(video_files)
   sYUVs = RGB_to_sYUVs(cap)
   #---  np.shape(sYUVs) = (10, 384, 512)
-  scYUVs = sYUVs_to_scYUVs(sYUVs)
-  #---  np.shape(scYUVs) = (10, 6, 128, 256)
+  CsYUVs = sYUVs_to_CsYUVs(sYUVs)
+  #---  np.shape(CsYUVs) = (10, 6, 128, 256)
 
-  #for i in tqdm(range(len(scYUVs) - 1)):
+  #for i in tqdm(range(len(CsYUVs) - 1)):
   for i in tqdm(range(9)):
-    inputs = [np.vstack(scYUVs[i:i+2])]
+    inputs = [np.vstack(CsYUVs[i:i+2])]
     #---  np.shape(inputs) = (1, 12, 128, 256)
 
     ret, frame = cap.read()
@@ -76,15 +76,15 @@ if __name__ == "__main__":
     cv2.imshow("video (874x1164)", frame)
     cv2.waitKey(500)  # Wait a 0.5 second (for testing)
 
-    # Convert YUV420 to RGB (for testing), applies BT.601 "Limited Range" conversion.
-    #rgb = cv2.cvtColor(scYUVs[i], cv2.COLOR_YUV2RGB_I420)  # Error
-    rgb = cv2.cvtColor(sYUVs[i], cv2.COLOR_YUV2RGB_I420)
-    cv2.imshow("yuv2rgb (256x512)", rgb)
-    cv2.waitKey(500)  # Wait a 0.5 second (for testing)
-
     # Convert YUV420 to Grayscale
     gray = cv2.cvtColor(sYUVs[i], cv2.COLOR_YUV2GRAY_I420)
     cv2.imshow("yuv2gray", gray)
+    cv2.waitKey(500)  # Wait a 0.5 second (for testing)
+
+    # Convert YUV420 to RGB (for testing), applies BT.601 "Limited Range" conversion.
+    #rgb = cv2.cvtColor(CsYUVs[i], cv2.COLOR_YUV2RGB_I420)  # Error
+    rgb = cv2.cvtColor(sYUVs[i], cv2.COLOR_YUV2RGB_I420)
+    cv2.imshow("yuv2rgb (256x512)", rgb)
     cv2.waitKey(500)  # Wait a 0.5 second (for testing)
 
   print("#---  np.shape(inputs) =", np.shape(inputs))
